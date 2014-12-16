c300a49f90e8d03e937e5378a9b70a0e7df3c3f2
unknown <aprieto@hxws-aprieto.hexacta.com>
2014-12-16 17:03:31 -0300

    Added Jasmine & Karma for testing

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -14,3 +14,8 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+angular.module('myApp', [
+	'myApp.controllers'
+]);
