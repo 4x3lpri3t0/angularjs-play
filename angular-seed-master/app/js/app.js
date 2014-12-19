661cf686194b22ff344e0dbcc3d5b5c2791d2aea
unknown <aprieto@hxws-aprieto.hexacta.com>
2014-12-19 19:13:27 -0300

    practice practice practice

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -2,20 +2,26 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+// angular.module('myApp', [
+//   'ngRoute',
+//   'myApp.filters',
+//   'myApp.services',
+//   'myApp.directives',
+//   'myApp.controllers'
+// ]).
+// config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
 
 
 angular.module('myApp', [
   'myApp.controllers'
 ]);
+
+// Callback passed to angular.module('myApp').run() gets called when all the modules are loaded
+angular.module('myApp').run(function($rootScope){
+	$rootScope.title = 'Famous Books';
+	$rootScope.name = 'Root Scope';
+})
--- angular-seed-master/app/js/controllers.js
+++ angular-seed-master/app/js/controllers.js
@@ -2,22 +2,13 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
-
-angular.module('myApp.controllers', []).
-  controller('FinanceController', function($scope) {
+angular.module('myApp.controllers', [])
+  .controller('FinanceController', function($scope) {
   	$scope.salary = 0;
   	$scope.percentage = 0;
   	$scope.result = function () {
   		return $scope.salary * $scope.percentage * 0.01;
-  	};
-  })
+  	}})
   .controller('GreetingController', function($scope){
     this.now = new Date();
     this.helloMessages = ['Hello', 'Bonjour', 'Hola', 'Ciao', 'Hallo'];
@@ -25,6 +16,15 @@ angular.module('myApp.controllers', []).
     this.getRandomHelloMessage = function () {
     	var randomMsgIndex = parseInt((Math.random() * this.helloMessages.length));
     	this.greeting = this.helloMessages[randomMsgIndex];
-    }
-
-  });
+    }})
+  .controller('SiteController', 
+    function($scope){
+      $scope.publisher = 'SitePoint';
+	  $scope.type = 'Web Development';
+	  $scope.name = 'Scope for SiteController';
+	})
+  .controller('BookController',
+    function($scope){
+      $scope.books = ['Jump Start HTML5', 'Jump Start CSS', 'Jump Start Responsive Web Design'];
+      $scope.name = 'Scope for BookController';
+});
